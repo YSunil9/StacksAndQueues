01aa6818ecc3e29142f17c44df89322e59b189fb
Ysunil9 <sunil.pcs24@gmail.com>
2022-12-03 23:35:41 +0530

    Welcomr To UC1

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues Problem\n");
+            stackAndQueue stack = new stackAndQueue();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("\nThe Elements Are Present In The Stack");
+            stack.Display();
+        }
+    }
+}
